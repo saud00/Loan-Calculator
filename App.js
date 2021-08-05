99cac7d41444b5518194c06f1699deb96696fd9e
Saud <gomail2017@gmail.com>
2021-08-05 23:45:50 +0500

    added UI things, project finish

--- App.js
+++ App.js
@@ -1,39 +1,81 @@
 // Query Selector
-const calculate = document.querySelector("#calculate");
+const submit = document.getElementById("loan-form");
 
 const loanAmount = document.querySelector("#loan-amount");
-const years = document.querySelector("#years");
 const interestRate = document.querySelector("#interest-rate");
+const years = document.querySelector("#years");
 const rMonthly = document.querySelector("#r-monthly");
 const rTotal = document.querySelector("#r-total");
 const rInterest = document.querySelector("#r-totalInterest");
+const resultDiv = document.getElementById("2");
+
+const loading = document.getElementById("loading");
+
+function dangerError(e) {
+  e.className = "form-control is-invalid";
+  console.log("triggered");
+  const danger = document.createElement("div");
+  const dangertext = document.createElement("small");
+
+  dangertext.appendChild(document.createTextNode(`Enter correct ${e.id} `));
+  dangertext.className = "text-danger";
+  dangertext.id = "passwordHelp";
+  danger.appendChild(dangertext);
+  submit.insertBefore(danger, e.parentElement.parentElement);
+}
 
 let loanValue, interestValue, yearsValue;
 
 document.addEventListener("input", clicked);
 function clicked(e) {
-  if (e.target.id == "loan-amount") return (loanValue = e.target.value);
-  if (e.target.id == "interest-rate") return (interestValue = e.target.value);
-  if (e.target.id == "years") return (yearsValue = e.target.value);
+  if (e.target.id == "loan-amount") {
+    loanAmount.className = "form-control is-valid";
+
+    return (loanValue = e.target.value);
+  }
+  if (e.target.id == "interest-rate") {
+    interestRate.className = "form-control is-valid";
+    return (interestValue = e.target.value);
+  }
+
+  if (e.target.id == "years") {
+    years.className = "form-control is-valid";
+    return (yearsValue = e.target.value);
+  }
 }
 
-calculate.addEventListener("click", calculateFormula);
+submit.addEventListener("submit", calculateFormula);
 
 function calculateFormula(e) {
+  e.preventDefault();
   const principal = parseFloat(loanValue);
   const calculatedInterest = parseFloat(interestValue) / 100 / 12;
   const calculatedPayments = parseFloat(yearsValue) * 12;
 
-  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-  const monthly = (principal * x * calculatedInterest) / (x - 1);
+  let x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  let monthly = (principal * x * calculatedInterest) / (x - 1);
   console.log(x, monthly);
   if (isFinite(monthly)) {
-    rMonthly.value = monthly.toFixed(2);
-    rTotal.value = (monthly * calculatedPayments).toFixed(2);
-    rInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+    loading.className = "d-block mx-auto";
+    resultDiv.className = "d-none";
+    setTimeout(function () {
+      loading.className = "d-none";
+      resultDiv.className = "d-block";
+      rMonthly.value = monthly.toFixed(2);
+      rTotal.value = (monthly * calculatedPayments).toFixed(2);
+      rInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+    }, 1000);
   } else {
-    console.log("abs");
-    document.querySelector(".invalid-feedback").innerHTML =
-      "Please Enter correct values";
+    if (loanValue == null) {
+      dangerError(loanAmount);
+    }
+    if (interestValue == null) {
+      dangerError(interestRate);
+    }
+    if (yearsValue == null) {
+      dangerError(years);
+    }
   }
 }
+
+function timeoutFunction() {}
